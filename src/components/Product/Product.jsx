2cc3585aeb3a4c2d78b1042c527e5a26d51dda85
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-06-09 13:25:28 -0700

    Conditional Rednering

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,13 +1,25 @@
 import './Product.css'
 
 const Product = props => {
+
+    // Imperative Style
+    // let decrement = <button onClick={props.decrement}>-</button>
+
+    // if(props.amount <= 0 ){
+    //     decrement = undefined
+    // }
+
     return (
         <div className='Product'>
             <h2>{props.name}</h2>
             <div className='Count'>
                 <button onClick={props.increment}>+</button>
                 <h4>{props.amount}</h4>
-                <button onClick={props.decrement}>-</button>
+                {props.amount > 0 ? (
+                    <button onClick={props.decrement}>-</button>
+                ) : (
+                    undefined
+                )}
             </div>
         </div>
     )
